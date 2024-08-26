5085d444281695e74af5dd2c15439fec7b2654a9
Omarokmm <omar.kmh.96@gmail.com>
2024-08-26 13:49:41 +0400

    filter date to starting cases

--- src/components/Layout/UserProfile.js
+++ src/components/Layout/UserProfile.js
@@ -273,6 +273,72 @@ const searchByDate = (e) => {
         setCasesUser(buffCasesUser);
     }
 }
+const searchStartByDate = (e) => {
+    const date = e.target.value
+    setStartDat(date)
+    if(date != "")
+    {
+      console.log("date")
+      if(userData.isAdmin ? userData.departments[0].name === "CadCam" : departments[0].name === "CadCam"){
+        const filteredCases = buffCasesStartingUser.filter((item) => {
+          return  _global.formatDateToYYYYMMDD(item.cadCam.actions[item.cadCam.actions.length  - 1]?.dateStart) === date;
+      });
+      setStartCases(filteredCases);
+      }
+      if(userData.isAdmin ? userData.departments[0].name === "Caramic" : departments[0].name === "Caramic"){
+        const filteredCases = buffCasesStartingUser.filter((item) => {
+          return  _global.formatDateToYYYYMMDD(item.ceramic.actions[item.ceramic.actions.length  - 1]?.dateStart) === date;
+      });
+      setStartCases(filteredCases);
+      }
+      if(userData.isAdmin ? userData.departments[0].name === "Fitting" : departments[0].name === "Fitting"){
+        const filteredCases = buffCasesStartingUser.filter((item) => {
+          if(item.fitting.actions.length > 0)
+          return  _global.formatDateToYYYYMMDD(item.fitting.actions[item.fitting.actions.length  - 1]?.dateStart) === date;
+      });
+      
+      setStartCases(filteredCases);
+      }
+      if(userData.isAdmin ? userData.departments[0].name === "Plaster" : departments[0].name === "Plaster"){
+        const filteredCases = buffCasesStartingUser.filter((item) => {
+          return  _global.formatDateToYYYYMMDD(item.plaster.actions[item.plaster.actions.length  - 1]?.dateStart) === date;
+      });
+      
+      setStartCases(filteredCases);
+      } 
+      if(userData.isAdmin ? userData.departments[0].name === "Reception" : departments[0].name === "Reception"){
+        const filteredCases = buffCasesStartingUser.filter((item) => {
+          return  _global.formatDateToYYYYMMDD(item.receptionPacking.actions[item.receptionPacking.actions.length  - 1]?.dateStart) === date;
+      });
+      
+      setStartCases(filteredCases);
+      }
+      if(userData.isAdmin ? userData.departments[0].name === "Marketing" : departments[0].name === "Marketing"){
+        const filteredCases = buffCasesStartingUser.filter((item) => {
+          return  _global.formatDateToYYYYMMDD(item.designing.actions[item.designing.actions.length  - 1]?.dateStart) === date;
+      });
+      
+      setStartCases(filteredCases);
+      }
+      if(userData.isAdmin ? userData.departments[0].name === "QC" : departments[0].name === "QC"){
+        const filteredCases = buffCasesStartingUser.filter((item) => {
+          return  _global.formatDateToYYYYMMDD(item.qualityControl.actions[item.qualityControl.actions.length  - 1]?.dateStart) === date;
+      });
+      
+      setStartCases(filteredCases);
+      }
+      if(userData.isAdmin ? userData.departments[0].name === "Drivers" : departments[0].name === "Drivers"){
+        const filteredCases = buffCasesStartingUser.filter((item) => {
+          return  _global.formatDateToYYYYMMDD(item.delivering.actions[item.delivering.actions.length  - 1]?.dateStart) === date;
+      });
+      
+          setStartCases(buffCasesStartingUser);
+      }
+    }
+    else {
+        setCasesUser(buffCasesUser);
+    }
+}
 const searchByEndDate = (e) => {
   const date = e.target.value
   const start = _global.formatDateToYYYYMMDD(startDate);
@@ -737,7 +803,7 @@ const getHoldingreason = (item) => {
       tabIndex="0"
       >
       <div className="row">
-        <div className="col-10 mb-3 " >
+        <div className="col-7 mb-3 " >
         <input
             type="text"
             name="searchTextStart"
@@ -745,7 +811,19 @@ const getHoldingreason = (item) => {
             placeholder="Search by name | case number | case type "
             value={searchTextStart}
             onChange={(e) => searchByName(e.target.value,"Start")}
-            />        </div>
+            />       
+        </div>
+        {/* Start Date */}
+        <div className="col-lg-3 ">
+        <div className="form-group">
+            <input
+            type="date"
+            className="form-control"
+            placeholder="Select Date"
+            onChange={(e) => searchStartByDate(e)}
+            />
+            </div>
+        </div>
         <div className="col-2 mb-3 print-btn" >
         <button className="btn btn-sm btn-primary " onClick={() => {handleClick("Starting Cases");handlePrint1(); }}> <i class="fas fa-print"></i> print</button>
       </div>
@@ -866,11 +944,11 @@ const getHoldingreason = (item) => {
             value={searchTextHold}
             onChange={(e) => searchByName(e.target.value,"Pause")}
             />        
-          </div>
-          <div className="col-2 mb-3 print-btn" >
+        </div>
+        <div className="col-2 mb-3 print-btn" >
               <button className="btn btn-sm btn-primary " onClick={() => {handleClick("Holding Cases");handlePrint2(); }}> <i class="fas fa-print"></i> print</button>
             </div>
-          </div> 
+        </div> 
         
     <div ref={userRef2} style={{width:"100%"}}>
         {pauseCases?.length > 0 &&
